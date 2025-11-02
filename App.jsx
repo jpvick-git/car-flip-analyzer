7a34d938330d975e18d7856154260fa8c7e8858f
jpvick-git <jpvick@gmail.com>
2025-11-01 19:06:44 -0500

    1

--- App.jsx
+++ App.jsx
@@ -30,7 +30,8 @@ console.log("🚀 Fresh Vercel build deployed!");
     const fetchCarsFromDB = async () => {
       setLoading(true);
       try {
-        const res = await fetch("/api/cars/with_estimates");
+        const res = await fetch("http://45.55.43.140/cars/with_estimates");
+
         const data = await res.json();
         if (data && data.cars) {
           const cars = data.cars;

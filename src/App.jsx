353b8b11ffeafdf2f3ce2a1d8adf58bc088bf221
jpvick-git <jpvick@gmail.com>
2025-11-03 11:07:21 -0600

    2

--- src/App.jsx
+++ src/App.jsx
@@ -177,7 +177,7 @@ return (
 
 
     {/* MAIN CONTENT */}
-    <main className="flex-1 overflow-y-auto p-6">
+    <main className="flex-1 p-6 pt-[180px]">
       {loading && (
         <p className="text-center text-gray-400 mt-10">
           ⏳ Loading vehicles...

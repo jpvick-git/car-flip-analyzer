92eb1fc68739b7464c883c27cb5256348c8e437d
jpvick-git <jpvick@gmail.com>
2025-11-25 12:20:30 -0600

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import CarCard from "../components/CarCard"; // FIXED PATH
+import CarCard from "../components/CarCard"; 
+import Header from "../components/Header";
+
 
 export default function Dashboard({ openCSVModal }) {
   const [cars, setCars] = useState([]);

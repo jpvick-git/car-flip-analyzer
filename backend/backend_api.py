871e59c7501f1794c45b1ae5de7c7c8548439fb7
jpvick-git <jpvick@gmail.com>
2025-11-26 16:12:54 -0600

    2

--- backend/backend_api.py
+++ backend/backend_api.py
@@ -151,7 +151,7 @@ from .auth import router as auth_router
 from backend.routes.manual_vehicle import router as manual_vehicle_router
 from backend.routes.carquery import router as carquery_router
 from backend.routes.nhtsa import router as nhtsa_router
-from backend.routers.specs import router as specs_router
+from backend.routes.specs import router as specs_router
 
 
 
